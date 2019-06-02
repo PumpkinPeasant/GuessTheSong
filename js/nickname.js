9b53206df842b4c5b20fde6ff18dcaec7802536b
PumpkinPeasant <50501903+PumpkinPeasant@users.noreply.github.com>
2019-06-03 00:04:20 +0300

    Add files via upload

---
+++ js/nickname.js
@@ -0,0 +1,17 @@
+﻿
+var nickname = prompt('Введите никнейм');
+var i = 0;
+var str = 'Ваш никнейм:  '
+while (i == 0)
+{
+	if(nickname.length <= 12)
+	{
+		alert(str + nickname);
+		i++;
+	}
+	else
+	{
+		alert('Никнейм должен состоять не более чем из 12 символов');
+		nickname = prompt('Введите никнейм');
+	}
+}
---
+++ js/script.js
@@ -0,0 +1,15 @@
+﻿function randombg(){
+  var random= Math.floor(Math.random() * 8) + 0;
+  var bigSize = ["url('img/1.jpg')",
+                 "url('img/2.jpg')",
+                 "url('img/3.jpg')",
+                 "url('img/4.jpg')",
+                 "url('img/5.jpg')",
+				 "url('img/6.jpg')",
+				 "url('img/7.jpg')",
+                 "url('img/8.jpg')"];
+  document.getElementById("wrapper").style.backgroundImage=bigSize[random];
+}
+function login(){
+  document.getElementById("wrapper").style.filter="blur(0px)";
+}
